91fbdbdff468360a49c0bbc3c5387764d7ec19ed
Masuri <psh0258@gmail.com>
2022-03-29 00:10:03 +0900

    AccountsCountroller 추가

--- NolowaBackendDotNet/Startup.cs
+++ NolowaBackendDotNet/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using NolowaBackendDotNet.Models.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
