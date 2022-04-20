75ae1dd139e35c96d9d298b65589635f2b697477
Masuri <psh0258@gmail.com>
2022-04-20 22:45:14 +0900

    프로필파일 올바로 전달 못하는 문제 해결

--- NolowaBackendDotNet/Models/DTOs/ProfileImageDTO.cs
+++ NolowaBackendDotNet/Models/DTOs/ProfileImageDTO.cs
@@ -9,10 +9,6 @@ namespace NolowaBackendDotNet.Models.DTOs
     public class ProfileImageDTO : NolowaImage
     {
         public long Id { get; set; }
-        [JsonProperty("hash")]
-        public string FileHash { get; set; }
-        public string Url { get; set; }
-
         //public virtual ICollection<Account> Accounts { get; set; }
     }
 }
