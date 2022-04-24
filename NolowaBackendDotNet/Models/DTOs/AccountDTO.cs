04b6d6f8880632f28541000fb6bee73cbd081979
Masuri <psh0258@gmail.com>
2022-04-24 22:39:21 +0900

    계정에 이름 올바로 가져갈 수 있도록 변경

--- NolowaBackendDotNet/Models/DTOs/AccountDTO.cs
+++ NolowaBackendDotNet/Models/DTOs/AccountDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace NolowaBackendDotNet.Models.DTOs
     {
         public long Id { get; set; }
         public string AccountId { get; set; } = string.Empty;
+        [JsonProperty("name")]
         public string AccountName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public DateTime InsertDate { get; set; }
