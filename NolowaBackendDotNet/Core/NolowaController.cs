cfefd201dc9060c2f7141581ed6fda84655d56af
Masuri <psh0258@gmail.com>
2022-04-29 12:32:14 +0900

    Swagger가 올바로 로드 될 수 있도록 Base Class의 함수를 Swagger에 표시하지 않도록 protected로 변경

--- NolowaBackendDotNet/Core/NolowaController.cs
+++ NolowaBackendDotNet/Core/NolowaController.cs
@@ -11,14 +11,14 @@ namespace NolowaBackendDotNet.Core
 {
     public abstract class NolowaController : ControllerBase
     {
-        public long GetLoggedInUserAccountIDFromToken()
+        protected long GetLoggedInUserAccountIDFromToken()
         {
             bool tryResult = Request.Headers.TryGetValue("Authorization", out StringValues values);
 
             if (tryResult == false)
                 throw new InvalidOperationException("Header에서 Token을 찾을 수 없습니다.");
 
-            if(values.Count != 1)
+            if (values.Count != 1)
                 throw new InvalidOperationException("Header에 0개나 1개 초과의 Token이 있습니다.");
 
             var token = values[0].Replace("Bearer ", "");
