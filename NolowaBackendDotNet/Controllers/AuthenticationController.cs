4e62ab8d6adc9d64a13534691d1aa23e5286bc2f
Masuri <psh0258@gmail.com>
2022-05-02 00:14:34 +0900

    구글 로그인시 콜백 받는 API 생성

---
+++ NolowaBackendDotNet/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NolowaBackendDotNet.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NolowaBackendDotNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : NolowaController
+    {
+        [HttpGet("Social/Google/Callback/")]
+        public void GoogleCallback([FromQuery] string code)
+        {
+
+        }
+    }
+}
