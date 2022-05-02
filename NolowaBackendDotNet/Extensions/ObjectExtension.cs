69810e22be9198fbbe975e785de8d505db588254
Masuri <psh0258@gmail.com>
2022-05-03 01:03:49 +0900

    IsNull, IsNotNull Extension Method 추가

---
+++ NolowaBackendDotNet/Extensions/ObjectExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NolowaBackendDotNet.Extensions
+{
+    public static class ObjectExtension
+    {
+        public static bool IsNull(this object src)
+        {
+            return src == null;
+        }
+
+        public static bool IsNotNull(this object src)
+        {
+            return !src.IsNull();
+        }
+    }
+}
