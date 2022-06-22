21ba9ba5b369d7a74460a64ce30a821d2db541e6
Masuri <psh0258@gmail.com>
2022-06-22 20:15:47 +0900

    DirectMessage Table 추가

---
+++ NolowaBackendDotNet/Models/Context.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace NolowaBackendDotNet.Models
+{
+    public partial class Context : DbContext
+    {
+        public Context()
+        {
+        }
+
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<DirectMessage> DirectMessages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("name=ConnectionStrings:NolowaContext");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Korean_Wansung_CI_AS");
+
+            modelBuilder.Entity<DirectMessage>(entity =>
+            {
+                entity.ToTable("DirectMessage");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.InsertTime)
+                    .HasColumnType("date")
+                    .HasColumnName("INSERT_TIME");
+
+                entity.Property(e => e.Message)
+                    .IsRequired()
+                    .HasColumnName("MESSAGE");
+
+                entity.Property(e => e.ReceiverId).HasColumnName("RECEIVER_ID");
+
+                entity.Property(e => e.SenderId).HasColumnName("SENDER_ID");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ NolowaBackendDotNet/Models/DirectMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NolowaBackendDotNet.Models
+{
+    public partial class DirectMessage
+    {
+        public long Id { get; set; }
+        public long SenderId { get; set; }
+        public long ReceiverId { get; set; }
+        public string Message { get; set; }
+        public DateTime InsertTime { get; set; }
+    }
+}
