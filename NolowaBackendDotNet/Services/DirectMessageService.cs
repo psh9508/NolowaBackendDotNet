54ec5b157851beaebef3151d649d870384d1b416
Masuri <psh0258@gmail.com>
2022-08-11 00:15:03 +0900

    대화기록 가져오는 로직 리펙토링

--- NolowaBackendDotNet/Services/DirectMessageService.cs
+++ NolowaBackendDotNet/Services/DirectMessageService.cs
@@ -56,116 +56,97 @@ namespace NolowaBackendDotNet.Services
                                        .OrderBy(x => x.InsertTime);
         }
 
-        public async Task<IEnumerable<PreviousDialogListItem>> GetPreviousDialogList(long senderId)
+        public async Task<IEnumerable<PreviousDialogListItem>> GetPreviousDialogList(long loginUserId)
         {
             //SELECT TOP 10 *
-		    //FROM ( SELECT MAIN.ID, MAIN.INSERT_TIME
-		    //       FROM (SELECT G.*, RECEIVER_ID, SENDER_ID, INSERT_TIME	  	
-	        //             FROM (SELECT MAX(ID) AS ID
-		    //       	             , SUM(CASE(IS_READ) WHEN 1 THEN 0 ELSE 1 END) AS NEW_MESSAGE_CNT
-		    //                   FROM[Nolowa].[dbo].[DirectMessage]
-		    //                   WHERE SENDER_ID = 2
-		    //                   GROUP BY RECEIVER_ID) AS G
-	        //             JOIN [Nolowa].[dbo].[DirectMessage] AS M ON M.ID = G.ID AND M.RECEIVER_ID = 3) AS MAIN
-		    //       JOIN [dbo].[Account] AS A ON A.ID = MAIN.RECEIVER_ID			
-	        //	   UNION ALL
-		    //	   SELECT MAIN.ID, MAIN.INSERT_TIME
-		    //       FROM (SELECT G.*, RECEIVER_ID, SENDER_ID, INSERT_TIME	  
-	        //             FROM (SELECT MAX(ID) AS ID
-		    //       	             , SUM(CASE(IS_READ) WHEN 1 THEN 0 ELSE 1 END) AS NEW_MESSAGE_CNT
-		    //                   FROM[Nolowa].[dbo].[DirectMessage]
-		    //                   WHERE SENDER_ID = 2
-		    //                   GROUP BY RECEIVER_ID) AS G
-	        //             LEFT JOIN [Nolowa].[dbo].[DirectMessage] AS M ON M.ID = G.ID AND M.SENDER_ID = 2) AS MAIN
-		    //       JOIN [dbo].[Account] AS A ON A.ID = MAIN.SENDER_ID
-		    //	  ) LAST
-		    //ORDER BY INSERT_TIME DESC
+            //FROM ( SELECT MAIN.ID, MAIN.INSERT_TIME
+            //       FROM (SELECT G.*, RECEIVER_ID, SENDER_ID, INSERT_TIME	  	
+            //             FROM (SELECT MAX(ID) AS ID
+            //       	             , SUM(CASE(IS_READ) WHEN 1 THEN 0 ELSE 1 END) AS NEW_MESSAGE_CNT
+            //                   FROM[Nolowa].[dbo].[DirectMessage]
+            //                   WHERE SENDER_ID = 2
+            //                   GROUP BY RECEIVER_ID) AS G
+            //             JOIN [Nolowa].[dbo].[DirectMessage] AS M ON M.ID = G.ID AND M.RECEIVER_ID = 3) AS MAIN
+            //       JOIN [dbo].[Account] AS A ON A.ID = MAIN.RECEIVER_ID			
+            //	   UNION ALL
+            //	   SELECT MAIN.ID, MAIN.INSERT_TIME
+            //       FROM (SELECT G.*, RECEIVER_ID, SENDER_ID, INSERT_TIME	  
+            //             FROM (SELECT MAX(ID) AS ID
+            //       	             , SUM(CASE(IS_READ) WHEN 1 THEN 0 ELSE 1 END) AS NEW_MESSAGE_CNT
+            //                   FROM[Nolowa].[dbo].[DirectMessage]
+            //                   WHERE SENDER_ID = 2
+            //                   GROUP BY RECEIVER_ID) AS G
+            //             LEFT JOIN [Nolowa].[dbo].[DirectMessage] AS M ON M.ID = G.ID AND M.SENDER_ID = 2) AS MAIN
+            //       JOIN [dbo].[Account] AS A ON A.ID = MAIN.SENDER_ID
+            //	  ) LAST
+            //ORDER BY INSERT_TIME DESC
 
-            var previousDialogList = new List<PreviousDialogListItem>();
-
-            var receivers = _context.DirectMessages.Where(x => x.SenderId == senderId)
-                                                   .Select(x => x.ReceiverId)
-                                                   .Distinct().ToList();
-
-            foreach (var receiver in receivers)
-            {
-                var send = _context.DirectMessages.Where(x => x.SenderId == senderId && x.ReceiverId == receiver)
-                                                    .GroupBy(dm => dm.ReceiverId)
-                                                    .Select(x => new
-                                                    {
-                                                        Id = x.Max(x => x.Id),
-                                                        NewMessageCount = x.Where(x => x.ReceiverId == senderId).Sum(x => x.IsRead ? 0 : 1)
-                                                    })
-                                                    .Join(_context.DirectMessages, x => x.Id, g => g.Id, (x, g) =>
-                                                        new
+            var idGroups = await _context.DirectMessages.Where(x => x.SenderId == loginUserId || x.ReceiverId == loginUserId) // 내가 보낸 것과 내가 받은 것 모두 가져온다.
+                                                        .GroupBy(x => new { x.SenderId, x.ReceiverId }, (key, group) => new
                                                         {
-                                                            ReceiverId = g.ReceiverId,
-                                                            Message = g.Message,
-                                                            Time = g.InsertTime,
-                                                            NewMessageCount = x.NewMessageCount,
-                                                        }
-                                                    ).Join(_context.Accounts, x => x.ReceiverId, a => a.Id, (x, a) =>
-                                                        new PreviousDialogListItem()
-                                                        {
-                                                            Account = _mapper.Map<AccountDTO>(new Account()
-                                                            {
-                                                                Id = a.Id,
-                                                                UserId = a.UserId,
-                                                                AccountName = a.AccountName,
-                                                                ProfileInfo = new ProfileInfo()
-                                                                {
-                                                                    ProfileImg = a.ProfileInfo.ProfileImg,
-                                                                },
-                                                            }),
-                                                            Message = x.Message,
-                                                            Time = x.Time,
-                                                            NewMessageCount = x.NewMessageCount,
-                                                        }
-                                                    ).ToList().SingleOrDefault();
-
-                var received = _context.DirectMessages.Where(x => x.SenderId == receiver && x.ReceiverId == senderId)
-                                                        .GroupBy(dm => dm.SenderId)
-                                                        .Select(x => new
-                                                        {
-                                                            Id = x.Max(x => x.Id),
-                                                            NewMessageCount = x.Where(x => x.ReceiverId == senderId).Sum(x => x.IsRead ? 0 : 1)
+                                                            SenderId = key.SenderId,
+                                                            ReceiverId = key.ReceiverId,
                                                         })
-                                                        .Join(_context.DirectMessages, x => x.Id, g => g.Id, (x, g) =>
-                                                            new
-                                                            {
-                                                                SenderId = g.SenderId,
-                                                                Message = g.Message,
-                                                                Time = g.InsertTime,
-                                                                NewMessageCount = x.NewMessageCount,
-                                                            }
-                                                        ).Join(_context.Accounts, x => x.SenderId, a => a.Id, (x, a) =>
-                                                            new PreviousDialogListItem()
-                                                            {
-                                                                Account = _mapper.Map<AccountDTO>(new Account()
-                                                                {
-                                                                    Id = a.Id,
-                                                                    UserId = a.UserId,
-                                                                    AccountName = a.AccountName,
-                                                                    ProfileInfo = new ProfileInfo()
-                                                                    {
-                                                                        ProfileImg = a.ProfileInfo.ProfileImg,
-                                                                    },
-                                                                }),
-                                                                Message = x.Message,
-                                                                Time = x.Time,
-                                                                NewMessageCount = x.NewMessageCount,
-                                                            }
-                                                             ).ToList().SingleOrDefault();
+                                                        .ToListAsync();
 
-                var sendTime = send?.Time;
-                var receivedTime = received?.Time;
+            var messageDataCollection = new List<DirectMessage>();
 
-                var addedData = sendTime.CompareTo(receivedTime) > 0 ? send : received;
+            foreach (var ids in idGroups)
+            {
+                var message = await _context.DirectMessages.OrderByDescending(x => x.InsertTime)
+                                                           .FirstOrDefaultAsync(x => x.SenderId == ids.SenderId && x.ReceiverId == ids.ReceiverId);
 
-                previousDialogList.Add(addedData);
+                if (message.IsNull())
+                    continue;
+
+                // 내가 보낸 바로 메시지면 저장하고 다음 데이터를 확인한다.
+                if (ids.SenderId == ids.ReceiverId)
+                {
+                    messageDataCollection.Add(message);
+                    continue;
+                }
+
+                var sameContextMessage = messageDataCollection.OrderByDescending(x => x.InsertTime).FirstOrDefault(x => x.SenderId == ids.ReceiverId && x.ReceiverId == ids.SenderId);
+
+                // 기존 데이터에서 같은 사람끼리 주고 받은 데이터가 있는지 확인한다.
+                if (sameContextMessage.IsNull())
+                {
+                    messageDataCollection.Add(message);
+                    continue;
+                }
+
+                // 같은 사람끼리 주고 받은 데이터가 있더면 시간순으로 최근 것을 저장하고 기존에 저장되어 있던것을 지워준다.
+                if (message.InsertTime.CompareTo(sameContextMessage.InsertTime) < 0)
+                {
+                    var alreadyInsertedMessage = messageDataCollection.Single(x => x.SenderId == ids.ReceiverId && x.ReceiverId == ids.SenderId);
+                    messageDataCollection.Remove(alreadyInsertedMessage);
+
+                    messageDataCollection.Add(sameContextMessage);
+                }
             }
 
-            return previousDialogList;
+            var finalDialog = messageDataCollection.Join(_context.Accounts.Include(dm => dm.ProfileInfo).ThenInclude(dm => dm.ProfileImg)
+                                                     , dm => dm.SenderId == loginUserId ? dm.ReceiverId : dm.SenderId
+                                                     , account => account.Id
+                                                     , (dm, account) => new PreviousDialogListItem()
+                                                     {
+                                                         Account = _mapper.Map<AccountDTO>(new Account()
+                                                         {
+                                                             Id = account.Id,
+                                                             UserId = account.UserId,
+                                                             AccountName = account.AccountName,
+                                                             ProfileInfo = new ProfileInfo()
+                                                             {
+                                                                 ProfileImg = account.ProfileInfo?.ProfileImg,
+                                                             },
+                                                         }),
+                                                         Message = dm.Message,
+                                                         Time = dm.InsertTime,
+                                                         NewMessageCount = GetUnreadMessageCountAsync(dm.SenderId, dm.ReceiverId).Result,
+                                                     }
+                                                     );
+
+            return finalDialog;
         }
 
         public async Task<int> GetUnreadMessageCountAsync(long userId)
@@ -178,8 +159,10 @@ namespace NolowaBackendDotNet.Services
 
         public async Task<int> GetUnreadMessageCountAsync(long senderId, long receiverId)
         {
-            return await _context.DirectMessages.Where(x => x.ReceiverId == senderId && x.SenderId == receiverId && x.IsRead == false)
-                                                .CountAsync();
+            //return await _context.DirectMessages.Where(x => x.ReceiverId == senderId && x.SenderId == receiverId && x.IsRead == false)
+            //                                    .CountAsync();
+            return await _context.DirectMessages.Where(x => x.ReceiverId == receiverId && x.SenderId == senderId && x.IsRead == false)
+                                               .CountAsync();
         }
 
         public async Task<int> SetReadAllMessageAsync(long senderId, long receiverId)
