497547ed551f8c3930933f9036d01ec0d32ae151
Masuri <psh0258@gmail.com>
2022-08-26 21:45:32 +0900

    간단한 확장함수 테스트 케이스 작성

---
+++ NolowaTest/Extensions/StringExtensionTests.cs
@@ -0,0 +1,58 @@
+﻿using NolowaBackendDotNet.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolowaTest.Extensions
+{
+    public class StringExtensionTests
+    {
+        [Test]
+        [TestCase("a")]
+        [TestCase(" a")]
+        [TestCase(".")]
+        public void IsValid_성공케이스(string src)
+        {
+            var actual = src.IsValid();
+
+            Assert.That(actual, Is.EqualTo(true));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("        ")]
+        public void IsValid_실패케이스(string src)
+        {
+            var actual = src.IsValid();
+
+            Assert.That(actual, Is.EqualTo(false));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("        ")]
+        public void IsNotValid_성공케이스(string src)
+        {
+            var actual = src.IsNotVaild();
+
+            Assert.That(actual, Is.EqualTo(true));
+        }
+
+        [Test]
+        [TestCase("a")]
+        [TestCase(" a")]
+        [TestCase(".")]
+        public void IsNotValid_실패케이스(string src)
+        {
+            var actual = src.IsNotVaild();
+
+            Assert.That(actual, Is.EqualTo(false));
+        }
+    }
+}
