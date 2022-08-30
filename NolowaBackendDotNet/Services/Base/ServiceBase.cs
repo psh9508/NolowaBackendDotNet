42bf9e7a370fbc389ad89ffa5a0102eb01ca7e77
Masuri <psh0258@gmail.com>
2022-08-30 18:18:38 +0900

    컴파일 오류 수정

--- NolowaBackendDotNet/Services/Base/ServiceBase.cs
+++ NolowaBackendDotNet/Services/Base/ServiceBase.cs
@@ -4,6 +4,8 @@ using NolowaBackendDotNet.Context;
 using NolowaBackendDotNet.Core;
 using NolowaBackendDotNet.Core.Base;
 using NolowaBackendDotNet.Core.CacheMonitor;
+using NolowaBackendDotNet.Core.Test;
+using NolowaBackendDotNet.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +15,15 @@ namespace NolowaBackendDotNet.Services.Base
 {
     public class ServiceBase<TService> : NolowaBase<TService>
     {
-        protected readonly NolowaContext _context;
-        protected readonly IMapper _mapper;
+        protected NolowaContext _context;
+        protected IMapper _mapper;
         protected readonly IJWTTokenProvider _jwtTokenProvider;
 
         public ServiceBase()
         {
+            if (TestHelper.IsTest)
+                return;
+
             _context = InstanceResolver.Instance.Resolve<NolowaContext>();
             _mapper = InstanceResolver.Instance.Resolve<IMapper>();
             _jwtTokenProvider = InstanceResolver.Instance.Resolve<IJWTTokenProvider>();
