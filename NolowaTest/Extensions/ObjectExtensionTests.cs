f44eaf3374526df2c0f2eb40daa279c0dacf290b
Masuri <psh0258@gmail.com>
2022-09-16 08:00:42 +0900

    Object Extension 테스트 추가

---
+++ NolowaTest/Extensions/ObjectExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using NolowaBackendDotNet.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolowaTest.Extensions
+{
+    class ObjectExtensionTests
+    {
+        [Test]
+        [TestCase(null)]
+        public void IsNull_성공케이드(object src)
+        {
+            var actual = src.IsNull();
+
+            Assert.That(actual, Is.EqualTo(true));
+        }
+    }
+}
