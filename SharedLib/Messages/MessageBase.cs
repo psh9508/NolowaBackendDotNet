9dfd48510a4c61a67240666ea6f4630da5c2eca3
Masuri <psh0258@gmail.com>
2023-03-01 00:03:23 +0900

    주고 받는 메시지의 배이스 데이터 추가

---
+++ SharedLib/Messages/MessageBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLib.Messages
+{
+    public enum MessageDestination
+    {
+        GATEWAY,
+        SERVER,
+    }
+
+    public class MessageBase
+    {
+        public string Origin { get; set; } = string.Empty;
+        public string Source { get; set; } = string.Empty;
+        public MessageDestination Target { get; set; }
+        public MessageDestination Destination { get; set; }
+        public string Function { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
