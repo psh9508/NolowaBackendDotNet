ae33661405465f887b1d0da56700679ee8f45cfd
masuri <psh0258@gmail.com>
2024-11-14 21:43:39 +0900

    Login http api 추가

---
+++ Gateway/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gateway.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost("v1/login")]
+        public async Task LoginAsync(string temp)
+        {
+            // rabbitmq로 서버를 들려서 로그인 결과를 받아와야 함
+        }
+    }
+}
