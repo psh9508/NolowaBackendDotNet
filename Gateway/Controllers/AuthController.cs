66ac0557bef5617c2cff3dd0b111b81b0e8a5122
masuri <psh0258@gmail.com>
2024-11-17 08:57:19 +0900

    AuthController 추가

--- Gateway/Controllers/AuthController.cs
+++ Gateway/Controllers/AuthController.cs
@@ -1,15 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NolowaNetwork.Protocols.Http;
 
 namespace Gateway.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class LoginController : ControllerBase
+    public class AuthController : ControllerBase
     {
+        private readonly IHttpProvider _httpProvider;
+
+        public AuthController(IHttpProvider httpProvider)
+        {
+            _httpProvider = httpProvider;
+        }
+
         [HttpPost("v1/login")]
         public async Task LoginAsync(string temp)
         {
             // rabbitmq로 서버를 들려서 로그인 결과를 받아와야 함
+            
         }
     }
 }
